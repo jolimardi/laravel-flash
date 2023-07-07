1e82a8b9290ac0819f96c81f4c7243a3d4853ce6
Emilien Hodee <hodeeemilien@gmail.com>
2023-07-07 18:29:57 +0200

    Correction function flash ?

--- src/FlashService.php
+++ src/FlashService.php
@@ -8,9 +8,7 @@ class FlashService {
 
     // Romain : Attention, tu dois pas renvoyer les messages ici, juste $session ! (qui représente le "moteur" utilisé pour les messages) -> Il va falloir rework les fonctions qui s'en servent... C'est la fonction getMessages() qui renvoie les messages
     protected static function getSessionService(): array {
-        $session = session();
-        $messages = $session->get('_messages', []);
-        return $messages;
+        return Session::get('_messages') ? Session::get('_messages') : [];
     }
 
     public static function getMessages(string $type = null, bool $remove = true): array {
@@ -27,13 +25,26 @@ class FlashService {
     // Ici, peut-être faire un check supplémentaire quand $type = null : si jamais on a un tableau avec $messages['error] = [] par exemple (pas de message dans un "sous-type")
     public static function has(string $type = null): bool {
         $session = self::getSessionService();
-        return $type ? isset($session[$type]) : !empty($session);
+
+        // si un $type est défini alors on cherche s'il y a des messages de ce type
+        if ($type) {
+            return isset($session[$type]) && !empty($session[$type]);
+        } else {
+            return !empty($session);
+        }
+
+        // Check s'il y a des messages dans la session en général
+        foreach ($session as $messages) {
+            if (!empty($messages)) {
+                return $session;
+            }
+        }
     }
 
     public static function message(string $type, string $message): void {
         $session = self::getSessionService();
         $session[$type][] = $message;
-        Session::put('_messages', $session); // Je crois que c'est mieux d'utiliser flash() ici, https://laravel.com/docs/10.x/session#flash-data
+        session()->flash('_messages', $session); // Je crois que c'est mieux d'utiliser flash() ici, https://laravel.com/docs/10.x/session#flash-data
     }
 
     public static function error(string $message): void {
@@ -58,7 +69,7 @@ class FlashService {
 
     /* Pour les views dans output() et renderMessage(), il va falloir que tu mettes les views dans le package (cf https://laravel.com/docs/10.x/packages#views) */
 
-    /*     public static function output(bool $remove = true): string {
+    public static function output(bool $remove = true): string {
         $session = self::getSessionService();
         $output = '';
 
@@ -78,7 +89,6 @@ class FlashService {
     }
 
     protected static function renderMessage(string $type, string $message) {
-        // Sans flash:: car il se trouve dans resources/view/components/output.blade.php
-        return view('components.output', compact('type', 'message'));
-    } */
+        return view('Flash::output', compact('type', 'message'))->render();
+    }
 }

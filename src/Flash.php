20eed725303bcef8815d49587032ddc7f3d3eae0
Emilien Hodee <hodeeemilien@gmail.com>
2023-07-07 18:26:49 +0200

    Commentaire Facade

--- src/Flash.php
+++ src/Flash.php
@@ -5,8 +5,11 @@ namespace JoliMardi\Flash;
 use Illuminate\Support\Facades\Facade;
 
 // Ca sert à quelque chose ce fichier ?
+// https://laravel.com/docs/10.x/facades#how-facades-work
+// Ce qui permet d'écrire Flash::has() au lieu de app('Flash')->has()
+// Il existe d'autre facade : Route, DB, Cache... comme un centre commercial ou on choisit son magasin(sa classe), c'est comme sa que je le vois ^^'
 class Flash extends Facade {
-    protected static function getFacadeAccessor() {
+    protected static function getFacadeAccessor(): string {
         return 'Flash';
     }
 }

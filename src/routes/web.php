d4fd938550c3a782d99e1c19ec9c2b5dc96a3d31
Emilien Hodee <hodeeemilien@gmail.com>
2023-08-02 17:47:10 +0200

    Route suppression

--- src/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-// MyVendor\contactform\src\routes\web.php
-Route::get('package', function () {
-    return 'Hello from the contact form package';
-});
